486115c087fb4cceb83c92c4873a6cd152b32442
philipsalvator <50589163+philipsalvator@users.noreply.github.com>
2019-06-17 01:53:36 +0200

    Test

--- src/encryption/mainFrame.java
+++ src/encryption/mainFrame.java
@@ -12,7 +12,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class mainFrame extends JFrame {
-//Test
+
     //1
     private static final String ALPHA_NUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private JPasswordField passwordField;
